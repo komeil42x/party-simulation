22e4d55588c54d1f429a6b0090ee240425c2a2d1
unknown <wos855@haw-hamburg.de>
2024-04-12 10:19:12 +0200

    Undid my mistakes
    Co-Authored-By: PhilippRiefer <66263398+PhilippRiefer@users.noreply.github.com>

---
+++ SimulationControl.java
@@ -0,0 +1,15 @@
+                     
+public class SimulationControl {
+
+	private Environment environment;
+	private Avatar[] avatar;
+	
+	public SimulationControl() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
