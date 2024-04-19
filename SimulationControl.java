6b8884d2e92dfcc733067debf1661322c1323e96
TomKreye <tom.kreye1@gmail.com>
2024-04-19 09:35:34 +0200

    Test
    Co-Authored-By: PhilippRiefer <66263398+PhilippRiefer@users.noreply.github.com>

--- SimulationControl.java
+++ SimulationControl.java
@@ -1,11 +1,8 @@
                      
 public class SimulationControl {
 
-	private Environment environment;
-	private Avatar[] avatar;
-	
 	public SimulationControl() {
-		jk
+		
 	}
 	
 	public static void main(String[] args) {
