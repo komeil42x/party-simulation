4e363a592c4935dd8e14aee067170e2442dac27d
TomKreye <tom.kreye1@gmail.com>
2024-04-26 11:57:45 +0200

    saving
    Co-Authored-By: PhilippRiefer <66263398+PhilippRiefer@users.noreply.github.com>

--- src/Avatar_Interface/AvatarInterface.java
+++ src/Avatar_Interface/AvatarInterface.java
@@ -3,7 +3,6 @@ package Avatar_Interface;
 import java.util.ArrayList;
 
 import Environment.SpaceType;
-import SimulationControl.SpaceInfo;
 
 public interface AvatarInterface {
 	abstract Direction yourTurn(ArrayList<SpaceType> si);
