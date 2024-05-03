2cbbaffb29be971b7c65732cc406e5436fd0747f
PhilippRiefer <66263398+PhilippRiefer@users.noreply.github.com>
2024-05-03 13:18:08 +0200

    In Progress; Loop through Classes in PersonalAvatars package
    Adding avatars dynamically based on classes in PersonalAvatars package.

--- src/PersonalAvatars/NasserAvatar.java
+++ src/PersonalAvatars/NasserAvatar.java
@@ -1,40 +1,32 @@
-// package PersonalAvatars;
-// import java.util.ArrayList;
-// import AvatarInterface.*;
-// import Environment.SpaceType;
+package PersonalAvatars;
+import java.util.ArrayList;
+import AvatarInterface.*;
+import Environment.*;
 
-// /*
-//  * Zufällige Bewegung vom Avatar
-//  * Bewegung vom Avatar in gezielte Richtung
-//  */
+/*
+ * Zufällige Bewegung vom Avatar
+ * Bewegung vom Avatar in gezielte Richtung
+ */
 
-// public class NasserAvatar extends SuperAvatar {
+public class NasserAvatar extends SuperAvatar {
 
-//     public NasserAvatar(int id) {
-//         super(id); 
-//     }
+    public NasserAvatar(int id) {
+        super(id); 
+    }
 
-//     @Override
-//     public Direction yourTurn(SpaceInfo[] si) {
-       
-//         int max = 4;
-// 		int min = 0;
+    @Override
+    public Direction yourTurn(ArrayList<SpaceInfo> si) {
+        int max = 4;
+		int min = 0;
    
-// 		int directionNumber = (int) (Math.random() * ((max - min) + 1) + min);
+		int directionNumber = (int) (Math.random() * ((max - min) + 1) + min);
 
-// 		switch (directionNumber) {
-// 			case 0: return Direction.LEFT;
-// 			case 1: return Direction.RIGHT;
-// 			case 2: return Direction.UP;
-// 			case 3: return Direction.DOWN;
-// 			default: return Direction.STAY;
-// 		}
-        
-//     }
-
-//     @Override
-//     public Direction yourTurn(ArrayList<SpaceType> si) {
-//         // TODO Auto-generated method stub
-//         throw new UnsupportedOperationException("Unimplemented method 'yourTurn'");
-//     }
-// }
+		switch (directionNumber) {
+			case 0: return Direction.LEFT;
+			case 1: return Direction.RIGHT;
+			case 2: return Direction.UP;
+			case 3: return Direction.DOWN;
+			default: return Direction.STAY;
+		}
+    }
+}
--- src/SimulationControl/SimulationControl.java
+++ src/SimulationControl/SimulationControl.java
@@ -3,30 +3,41 @@ package SimulationControl;
 import java.util.ArrayList;
 import Environment.*;
 import AvatarInterface.*;
+import java.io.File;
+import org.reflections.Reflections;
 
 public class SimulationControl {
 
 	private ArrayList<SuperAvatar> avatars = new ArrayList<SuperAvatar>();
 	private Environment environment;
-	
-	public SimulationControl() {
-		System.out.println("Hallo");
 
+	public SimulationControl() {
 		environment = new Environment();
 
-		for (int i = 0; i < 12; i++){
-			avatars.add(new TestAvatar(i));
+		Reflections reflections = new Reflections("PersonalAvatars");
+
+		for (Class<? extends SuperAvatar> personalAvatarClass : reflections.getSubTypesOf(SuperAvatar.class)) {
+			try {
+				SuperAvatar avatar = personalAvatarClass.getDeclaredConstructor().newInstance();
+				avatars.add(avatar);
+				System.out.println("Added " + avatar.getClass().getName());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		// avatars.add(new AvatarNasser(i+1));
+
+		// for (int i = 0; i < countFilesInFolder("src/PersonalAvatars"); i++){
+		// 	avatars.add(new [PersonalAvatarClass](i));// TODO: change [PersonalAvatarClass] to the actual Avatar classes in the PersonalAvatars package
+
+		// }
 	}
 	
 	public void loopThroughAvatars() {
 		for (SuperAvatar avatar : avatars) {
 			ArrayList<SpaceInfo> si = environment.getAdjacentToAvatar(avatar.getAvatarID());
 			Direction dir = avatar.yourTurn(si);
-			// boolean hasMoved = environment.moveAvatar(avatar.getAvatarID(), dir);
-			// avatars.setHasMoved(hasMoved);
-			// Avatar mitteilen, ob er sich tatsächlich bewegt hat: hasMoved
+			boolean hasMoved = environment.moveAvatar(avatar.getAvatarID(), dir);
+			avatar.setHasMoved(hasMoved);
 		}
 	}
 	
